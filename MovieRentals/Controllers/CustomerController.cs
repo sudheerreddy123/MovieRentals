f0b67ccc7ea1e32e9ef6cf36836d16f6a59c0377
sudheer <sudheer9105@gmail.com>
2018-01-20 22:10:41 -0500

    MovieRentalsaddedwith basic info

---
+++ MovieRentals/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRentals.Models;
+
+namespace MovieRentals.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Random()
+        {
+            var customer = new Customer { id = 1, name = "Sudheer" };
+            return View(customer);
+        }
+    }
+}
---
+++ MovieRentals/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using MovieRentals.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRentals.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult GetMovies()
+        {
+            var movie = new Movies() { Name = "Sudheer" };
+            return View(movie);
+        }
+    }
+}
